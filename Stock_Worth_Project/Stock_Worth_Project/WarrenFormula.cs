a04f0924880aa8b102386c7a898f50c54851b755
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-16 08:52:45 -0500

    Added new class called Warren Formula

---
+++ Stock_Worth_Project/Stock_Worth_Project/WarrenFormula.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Worth_Project
+{
+    class WarrenFormula
+    {
+        private readonly double D_E_Ratio;
+        private readonly double BookValue;
+        private readonly double MarketValue;
+        private readonly string Company;
+
+        //--Constructor
+        public WarrenFormula()
+        {
+
+        }
+        
+    }
+}
