96558747ef3cc22f83aaef082e7dd611ad0382bb
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-21 16:54:46 -0500

    Brief outline of what will be presented to the user

--- Stock_Worth_Project/Stock_Worth_Project/Program.cs
+++ Stock_Worth_Project/Stock_Worth_Project/Program.cs
@@ -10,9 +10,14 @@ namespace Stock_Worth_Project
     {
         static void Main(string[] args)
         {
-            
-        }
+            /*  Noel Mowatt, Jr.    */
 
-        
+            //------------------------Warren Buffet's 4 Rules------------------------------//
+            //--Rule 1 - Is the company managed by vigilant leaders?
+            //--Rule 2 - Does the company has long term prospects?
+            //--Rule 3 - Is the stock stable and understandable?
+            //--Rule 4 - Is the stock undervalued?
+            //-------------If all 4 rules are met, then stock is a buy------------//
+        }
     }
 }
