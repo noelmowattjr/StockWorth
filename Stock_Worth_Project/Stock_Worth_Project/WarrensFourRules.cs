d8e166b0af481cc21feb02c446faf402ac20f44c
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-21 16:53:36 -0500

    WarrenFourRules class created

---
+++ Stock_Worth_Project/Stock_Worth_Project/WarrensFourRules.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Worth_Project
+{
+    class WarrensFourRules
+    {
+        private bool HasVigilantLeaders;
+        private bool HasLongTermProspects;
+        private bool StockIsStableAndUnderstandable;
+        private bool IsUnderValued;
+    }
+}
