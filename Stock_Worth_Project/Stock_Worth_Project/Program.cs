91ab78b2ff4e0753aa4f000a50f8ccc0403bbdd5
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-23 13:22:17 -0500

    Added types of questions the user will answer

--- Stock_Worth_Project/Stock_Worth_Project/Program.cs
+++ Stock_Worth_Project/Stock_Worth_Project/Program.cs
@@ -13,10 +13,18 @@ namespace Stock_Worth_Project
             /*  Noel Mowatt, Jr.    */
 
             //------------------------Warren Buffet's 4 Rules------------------------------//
-            //--Rule 1 - Is the company managed by vigilant leaders?
+
+            //--Rule 1 - Is the company managed by vigilant leaders?--//
+            //--What is the D/E ratio and current ratio?
+
             //--Rule 2 - Does the company has long term prospects?
+            //--Are earnings sustainable? Is the company likely to be around 30 years from now?
+
             //--Rule 3 - Is the stock stable and understandable?
+            //--Is the debt to equity history stable and are you able to determine an intrinsic value accurately?
+
             //--Rule 4 - Is the stock undervalued?
+            
             //-------------If all 4 rules are met, then stock is a buy------------//
         }
     }
