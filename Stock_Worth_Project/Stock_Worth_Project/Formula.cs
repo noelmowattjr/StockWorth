1383ef666224e3c309ed185e21d9daff1d0789ac
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-24 08:51:03 -0500

    Created Interface with 3 methods for implementation

--- Stock_Worth_Project/Stock_Worth_Project/Formula.cs
+++ Stock_Worth_Project/Stock_Worth_Project/Formula.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Stock_Worth_Project
 {
-    class Formula
+    interface IEvaluate
     {
+        double DEratio();
+        double CurrentRatio();
+        bool Undervalued();
+    }
+
+    class Formula : IEvaluate
+    {
+        public double DEratio()
+        {
+            return 1.1;
+        }
+
+        public double CurrentRatio()
+        {
+            return 1.0;
+        }
+
+        public bool Undervalued()
+        {
+            return true;
+        }
     }
 }
