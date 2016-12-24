872257879b7da82e9805662cfa5dff97677cb36c
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-24 11:11:07 -0500

    Instantiated Formula class to call its methods

--- Stock_Worth_Project/Stock_Worth_Project/Program.cs
+++ Stock_Worth_Project/Stock_Worth_Project/Program.cs
@@ -12,7 +12,10 @@ namespace Stock_Worth_Project
         {
             /*  Noel Mowatt, Jr.    */
 
+            Formula formula = new Formula();
+
             //------------------------Warren Buffet's 4 Rules------------------------------//
+            double liabilities; double equity;
 
             //--Rule 1 - Is the company managed by vigilant leaders?--//
             //--What is the D/E ratio and current ratio?
@@ -20,13 +23,26 @@ namespace Stock_Worth_Project
             string company = Console.ReadLine();
 
             Console.Write("Enter the total liabilities for 10 years: ");
-            string liabilities = Console.ReadLine();
+            string entryLi = Console.ReadLine();
+
+            //--Validate and convert to double for liabilities entry
+            while (!(double.TryParse(entryLi, out liabilities)))
+            {
+                Console.Write("Invalid!\r\nPlease enter a numeric value for liabilities: ");
+                entryLi = Console.ReadLine();
+            }
 
             Console.Write("Enter the total equity for 10 years: ");
-            string equity = Console.ReadLine();
+            string entryEq = Console.ReadLine();
+            while (!(double.TryParse(entryEq, out equity)))
+            {
+                Console.Write("Invalid!\r\nPlease enter a numeric value for equity: ");
+                entryEq = Console.ReadLine();
+            }
 
             //--Calculate with custom method here
-
+            var debtTOequityRatio = Math.Round(formula.DEratio(liabilities, equity), 2);
+            Console.WriteLine("The D/E Ration for {0} is {1}", company, debtTOequityRatio);
 
             //--Rule 2 - Does the company has long term prospects?
             //--Are earnings sustainable? Is the company likely to be around 30 years from now?
