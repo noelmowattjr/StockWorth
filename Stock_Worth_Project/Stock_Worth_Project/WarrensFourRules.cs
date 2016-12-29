738d9de9eda553c3dae7352302baf6bc8e8dc72f
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-29 14:02:42 -0500

    Created constructor and getters and setters for class field variables

--- Stock_Worth_Project/Stock_Worth_Project/WarrensFourRules.cs
+++ Stock_Worth_Project/Stock_Worth_Project/WarrensFourRules.cs
@@ -12,5 +12,37 @@ namespace Stock_Worth_Project
         private bool HasLongTermProspects;
         private bool StockIsStableAndUnderstandable;
         private bool IsUnderValued;
+
+        //--Constructor
+        public WarrensFourRules(bool hasVigilantLeaders, bool hasLongTermProspects, bool stockStableUnderstandable, bool isUndervalued)
+        {
+            HasVigilantLeaders = hasVigilantLeaders; HasLongTermProspects = hasLongTermProspects;
+            StockIsStableAndUnderstandable = stockStableUnderstandable; IsUnderValued = isUndervalued;
+        }
+
+        //--------------Getters & Setters--------------------//
+        public bool _HasVigilantLeaders
+        {
+            get { return HasVigilantLeaders; }
+            set { HasVigilantLeaders = value; }
+        }
+
+        public bool _HasLongTermProspects
+        {
+            get { return HasLongTermProspects; }
+            set { HasLongTermProspects = value; }
+        }
+
+        public bool _StockIsStableAndUnderstandable
+        {
+            get { return StockIsStableAndUnderstandable; }
+            set { StockIsStableAndUnderstandable = value; }
+        }
+
+        public bool _IsUnverValued
+        {
+            get { return IsUnderValued; }
+            set { IsUnderValued = value; }
+        }
     }
 }
