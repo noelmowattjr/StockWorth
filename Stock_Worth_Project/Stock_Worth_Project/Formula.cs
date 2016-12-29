5f359d4c1cbf9e3c9df96ae4d40a8f6618f1844b
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-29 14:07:34 -0500

    Modified definition of my Undervalued() method - included 2 parameters

--- Stock_Worth_Project/Stock_Worth_Project/Formula.cs
+++ Stock_Worth_Project/Stock_Worth_Project/Formula.cs
@@ -10,7 +10,7 @@ namespace Stock_Worth_Project
     {
         double DEratio(double li, double eq);
         double CurrentRatio(double assets, double li);
-        bool Undervalued();
+        bool Undervalued(double intrinsicValue, double marketValue);
     }
 
     class Formula : IEvaluate
