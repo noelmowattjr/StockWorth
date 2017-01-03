e1ad137d7d4b2d6f63eead267aab2464e7613160
Noel Mowatt <NoelMowattJr@gmail.com>
2017-01-03 09:33:54 -0500

    Prompting user for 3 data values in order to find the average change percent

--- Stock_Worth_Project/Stock_Worth_Project/Program.cs
+++ Stock_Worth_Project/Stock_Worth_Project/Program.cs
@@ -13,8 +13,37 @@ namespace Stock_Worth_Project
             /*  Noel Mowatt, Jr.    
                 Stock App Project */
 
-            //--Console.Write("Ent");
-            
+            //--Percentage increase for 10 years--//
+
+            //--Enter current book value
+            Console.Write("Current book value: ");
+            string currBookValue = Console.ReadLine(); int CurrBook;
+            while (!(int.TryParse(currBookValue, out CurrBook)))
+            {
+                Console.Write("Invalid!\r\nCurrent book value");
+                currBookValue = Console.ReadLine();
+            }
+
+            //--Enter old book value
+            Console.Write("Old book value: ");
+            string oldBookVaue = Console.ReadLine(); int OldBook;
+            while (!(int.TryParse(oldBookVaue, out OldBook)))
+            {
+                Console.Write("Invalid!\r\nOld book value: ");
+                oldBookVaue = Console.ReadLine();
+            }
+
+            //--Number of years between book values
+            Console.Write("Numbr of years between book value? "); int yrz;
+            string years = Console.ReadLine();
+            while (!(int.TryParse(years, out yrz)))
+            {
+                Console.Write("Invalid!\r\nEnter num of years between book values");
+                years = Console.ReadLine();
+            }
+
+            //--Average book value change
+   
         }
     }
 }
